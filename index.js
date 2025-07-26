a1be3fa458d17e60b67aa8b73be3ea59626e8d56
ActiveShayun <wevdeveloper0477gmail.com>
2025-07-26 08:42:58 +0600

    signin api create done

--- index.js
+++ index.js
@@ -13,10 +13,27 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5175',
     credentials: true
 }))
 app.use(cookieParser())
+
+
+const authenticate = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return req.status(401).json({ message: 'unauthorized' })
+    }
+
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+            return req.status(401).json({ message: 'unauthorized' })
+        }
+        console.log(' decoded user', decoded);
+        req.user = decoded
+        next()
+    })
+}
 
 
 
@@ -38,6 +55,22 @@ async function run() {
 
         const signUpCollection = client.db('login-db').collection('signup')
 
+        // token set in cookie
+        app.post('/set-cookie', (req, res) => {
+            const user = req.body;
+            const taken = jwt.sign(user, process.env.SECRET_TOKEN, {
+                expiresIn: '1h',
+            })
+
+            res.cookie('token', taken, {
+                httpOnly: true,
+                secure: false,
+                maxAge: 3600000,
+            })
+                .send({ success: true })
+        })
+
+        // signup api
         app.post('/signUp', async (req, res) => {
             const { email, password, photo, name } = req.body;
             console.log(email, password);
@@ -56,6 +89,23 @@ async function run() {
             })
             console.log('signUp result', result);
             res.status(201).json({ message: 'User created successfully' })
+        })
+
+        // login api
+        app.post('/signin', async (req, res) => {
+            const { email, password } = req.body;
+            const user = await signUpCollection.findOne({ email })
+            if (!user) {
+                return res.status(401).json({ message: 'invalid email' })
+            }
+            const isValid = await bcrypt.compare(password, user.password)
+            if (!isValid) {
+                return res.status(401).json({ message: 'invalid password' })
+            }
+            res.status(200).json({
+                success: true,
+                message: 'Logged in successfully done'
+            })
         })
 
 
